179acd824fbb014568129795f67abb84ef1ad8cc
qhy040404 <qhy040404@163.com>
2022-08-08 19:07:28 +0800

    Fix bug

--- dist/index.js
+++ dist/index.js
@@ -92,7 +92,7 @@ function splitData(data, delimiter, position) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const dataArray = data.split(delimiter);
-            core.debug(dataArray.toString());
+            core.debug(dataArray.length.toString());
             data = dataArray[position];
         }
         catch (e) {
